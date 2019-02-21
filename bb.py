808f6bfe0d17a9e91dad15cb674ec2c2298850f8
priyankakhawale <priyankakhawale@gmail.com>
2019-02-21 13:18:23 +0530

    branch commit

---
+++ bb.py
@@ -0,0 +1,17 @@
+ Volume in drive F is Aarna
+ Volume Serial Number is 48B5-DA77
+
+ Directory of F:\Django\Aarna\SCp
+
+21/02/2019  01:16 PM    <DIR>          .
+21/02/2019  01:16 PM    <DIR>          ..
+21/02/2019  01:16 PM                 0 bb.py
+07/01/2019  02:23 PM           135,168 db.sqlite3
+31/12/2018  02:23 PM               550 manage.py
+21/02/2019  01:14 PM               629 python.txt
+20/02/2019  12:46 PM               450 req.txt
+03/01/2019  05:47 PM    <DIR>          SCp
+03/01/2019  06:26 PM    <DIR>          student
+21/02/2019  01:05 PM               578 text.txt
+               6 File(s)        137,375 bytes
+               4 Dir(s)  10,587,303,936 bytes free
